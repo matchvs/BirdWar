cbfc728de909ddb4ac5116915fceca99b27d77b4
Mac <570768995@qq.com>
2018-08-15 16:22:42 +0800

    fix

--- assets/common/script/uiRoomList.js
+++ assets/common/script/uiRoomList.js
@@ -144,6 +144,8 @@ cc.Class({
     },
 
     onDestroy() {
+        clearInterval(this.roomRqId);
+
         clientEvent.off(clientEvent.eventType.getRoomListResponse, this.getRoomListResponse, this);
         clientEvent.off(clientEvent.eventType.joinRoomResponse, this.joinRoomResponse, this);
         clientEvent.off(clientEvent.eventType.getRoomListExResponse, this.getRoomListExResponse, this);
