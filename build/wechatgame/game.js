5412c33c5ff12c5dd1e3867df7f0b7fd1f4f48da
Mac <570768995@qq.com>
2018-09-10 15:50:02 +0800

    fix

--- build/wechatgame/game.js
+++ build/wechatgame/game.js
@@ -2,7 +2,7 @@ require('libs/weapp-adapter/index');
 var Parser = require('libs/xmldom/dom-parser');
 window.DOMParser = Parser.DOMParser;
 require('libs/wx-downloader.js');
-wxDownloader.REMOTE_SERVER_ROOT = "https://imgs.matchvs.com/static/tianziyou/birdWar";
+wxDownloader.REMOTE_SERVER_ROOT = "https://data.tianziyou.com/matchvsGamesRes/birdwar";
 wxDownloader.SUBCONTEXT_ROOT = "";
 require('src/settings.f84c6');
 require('main.af73d');
